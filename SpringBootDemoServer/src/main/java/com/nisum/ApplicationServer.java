00294eea44129aa415341aca975d0f59fba84e0f
siddaraja <siddaraja@192.168.1.140>
2017-02-20 22:06:51 +0530

    -- initial working project

---
+++ SpringBootDemoServer/src/main/java/com/nisum/ApplicationServer.java
@@ -0,0 +1,19 @@
+package com.nisum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ApplicationServer {
+	
+	public static void main(String[] args) {
+		
+	SpringApplication.run(ApplicationServer.class,args);
+		
+	}
+	
+	
+
+}
